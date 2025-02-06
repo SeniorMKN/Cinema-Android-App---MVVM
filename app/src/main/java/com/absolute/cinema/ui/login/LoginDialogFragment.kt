f85dde4ef559795ffa975ce2303f89c052b0c1b6
Salvio96 <venerusofamily@hotmail.it>
2025-02-06 18:31:55 +0100

    add login fragment and viewmodel

---
+++ app/src/main/java/com/absolute/cinema/ui/login/LoginDialogFragment.kt
@@ -0,0 +1,18 @@
+package com.absolute.cinema.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.absolute.cinema.R
+
+class LoginDialogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login_dialog, container, false)
+    }
+}
