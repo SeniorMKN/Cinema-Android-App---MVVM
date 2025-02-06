23d650e9591e253937f1af74b01589e306524990
Salvio96 <venerusofamily@hotmail.it>
2025-02-06 18:32:55 +0100

    add view binding and navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,18 @@ plugins {
 }
 
 android {
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     namespace = "com.absolute.cinema"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.absolute.cinema"
         minSdk = 26
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -42,6 +48,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/absolute/cinema/MainActivity.kt
+++ app/src/main/java/com/absolute/cinema/MainActivity.kt
@@ -3,11 +3,17 @@ package com.absolute.cinema
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.absolute.cinema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(view)
     }
 }
