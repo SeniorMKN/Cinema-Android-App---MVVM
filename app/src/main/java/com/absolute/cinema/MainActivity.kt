e20a9923967b4b42b0707114fe9f7d96a188e6d0
Salvio96 <venerusofamily@hotmail.it>
2025-02-07 18:23:11 +0100

    adding navigation graph and set navigation in activity

--- app/src/main/java/com/absolute/cinema/MainActivity.kt
+++ app/src/main/java/com/absolute/cinema/MainActivity.kt
@@ -3,15 +3,20 @@ package com.absolute.cinema
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
 import com.absolute.cinema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavHostController
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController as NavHostController
         val view = binding.root
         enableEdgeToEdge()
         setContentView(view)
