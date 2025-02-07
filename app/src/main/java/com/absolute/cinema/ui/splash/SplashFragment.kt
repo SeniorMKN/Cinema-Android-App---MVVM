03653fd6e1f63a6771b38a049f6b966b30578773
Salvio96 <venerusofamily@hotmail.it>
2025-02-07 18:24:07 +0100

    adding navigate direction to splash fragment

--- app/src/main/java/com/absolute/cinema/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.absolute.cinema.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.absolute.cinema.R
 
 class SplashFragment : Fragment() {
@@ -14,5 +17,13 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val navController = findNavController()
+            navController.navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+        }, 3000)
     }
 }
