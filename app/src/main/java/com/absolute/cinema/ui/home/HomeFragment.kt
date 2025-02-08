c6ede0c329eb85622ff72fece05f535e6422c3ee
jonathan.quaranta <jonathan.quaranta@synclab.it>
2025-02-08 09:50:49 +0100

    add login dialog fragment

--- app/src/main/java/com/absolute/cinema/ui/home/HomeFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/home/HomeFragment.kt
@@ -1,18 +1,39 @@
 package com.absolute.cinema.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.absolute.cinema.R
+import com.absolute.cinema.databinding.FragmentHomeBinding
+import com.absolute.cinema.databinding.LoginDialogBinding
+import com.absolute.cinema.ui.login.LoginDialogFragment
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logo.setOnClickListener {
+            LoginDialogFragment().show(parentFragmentManager, "LoginDialog")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
