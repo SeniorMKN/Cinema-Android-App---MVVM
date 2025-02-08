a512041381e78c45816f3d16279cc2755e7d2390
Salvio96 <venerusofamily@hotmail.it>
2025-02-08 23:14:00 +0100

    changing extends to DialogFragment

--- app/src/main/java/com/absolute/cinema/ui/login/LoginDialogFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/login/LoginDialogFragment.kt
@@ -1,38 +1,18 @@
 package com.absolute.cinema.ui.login
 
-import android.app.AlertDialog
-import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.absolute.cinema.R
-import com.absolute.cinema.databinding.FragmentLoginDialogBinding
 
 class LoginDialogFragment : DialogFragment() {
-    private var _binding: FragmentLoginDialogBinding? = null
-    private val binding get() = _binding!!
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        _binding = FragmentLoginDialogBinding.inflate(layoutInflater)
-
-        return AlertDialog.Builder(requireContext())
-            .setView(R.layout.fragment_login_dialog) // Set the custom view
-            .setCancelable(false) // Make dialog non-cancelable (optional)
-            .create()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.loginCv.setOnClickListener {
-            Log.i("LOGIN_CV", "login cv clicked")
-        }
-    }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login_dialog, container, false)
     }
 }
