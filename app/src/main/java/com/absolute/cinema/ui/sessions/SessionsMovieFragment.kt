cdaf25413c069d41be117acbbf3b503900a63286
Salvio96 <venerusofamily@hotmail.it>
2025-02-10 18:19:01 +0100

    adding drawables, shapes and 2 fragments sessions movie and tab movies. Changes to about fragment, login dialog and sessions fragment

---
+++ app/src/main/java/com/absolute/cinema/ui/sessions/SessionsMovieFragment.kt
@@ -0,0 +1,19 @@
+package com.absolute.cinema.ui.sessions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.absolute.cinema.R
+
+class SessionsMovieFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sessions_movie, container, false)
+    }
+}
