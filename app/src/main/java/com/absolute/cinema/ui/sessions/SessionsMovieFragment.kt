57cdeb14bd86efea8eddc1921351ca6f3a44028e
Salvio96 <venerusofamily@hotmail.it>
2025-02-12 11:07:33 +0100

    changes to sessions movies

--- app/src/main/java/com/absolute/cinema/ui/sessions/SessionsMovieFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/sessions/SessionsMovieFragment.kt
@@ -1,8 +1,10 @@
 package com.absolute.cinema.ui.sessions
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.absolute.cinema.data.model.TicketItemModel
 import com.absolute.cinema.databinding.FragmentSessionsMovieBinding
@@ -14,6 +16,15 @@ class SessionsMovieFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var recyclerViewAdapter: TicketRecyclerViewAdapter
     private lateinit var itemList: ArrayList<TicketItemModel>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSessionsMovieBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
