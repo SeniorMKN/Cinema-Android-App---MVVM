ac88e55b1b98f5849e133cb9c02d82a7f8620871
Salvio96 <venerusofamily@hotmail.it>
2025-02-18 20:05:09 +0100

    adjusting space

--- app/src/main/java/com/absolute/cinema/ui/city/CItyDialogFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/city/CItyDialogFragment.kt
@@ -22,7 +22,7 @@ class CItyDialogFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentCityDialogBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -30,7 +30,6 @@ class CItyDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initRecyclerView()
         super.onViewCreated(view, savedInstanceState)
-
 
     }
 
