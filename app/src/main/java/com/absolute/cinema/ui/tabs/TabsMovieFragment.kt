ae50243beca3fc20d85a2726bda8f8775289e39a
Salvio96 <venerusofamily@hotmail.it>
2025-03-02 18:55:27 +0100

    clean up code of the TabsMovieFragment

--- app/src/main/java/com/absolute/cinema/ui/tabs/TabsMovieFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/tabs/TabsMovieFragment.kt
@@ -5,14 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.absolute.cinema.R
+import com.absolute.cinema.data.remote.MoviesSharedViewModel
 import com.absolute.cinema.databinding.FragmentMovieTabsBinding
 import com.absolute.cinema.ui.about.AboutMovieFragment
 import com.absolute.cinema.ui.adapters.MyPagerAdapter
 import com.absolute.cinema.ui.sessions.SessionsMovieFragment
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class TabsMovieFragment : Fragment() {
@@ -20,11 +21,9 @@ class TabsMovieFragment : Fragment() {
     private var _binding: FragmentMovieTabsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var viewPager2: ViewPager2
     private lateinit var myAdapter: MyPagerAdapter
-    private lateinit var tabLayout: TabLayout
     private var tabsArray = arrayOf("About", "Sessions")
-
+    private val sharedViewModel: MoviesSharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,34 +36,39 @@ class TabsMovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        argumentsPassed()
+        setupView()
+        setupListeners()
+        initTabLayout()
+    }
 
-        binding.backArrowTv.setOnClickListener {
-            findNavController().popBackStack()
-        }
-
-        tabLayout = binding.tablayout
-        viewPager2 = binding.viewPager2
-        viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    private fun initTabLayout() {
+        binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         myAdapter = MyPagerAdapter(childFragmentManager, lifecycle)
         myAdapter.addFragmentToList(AboutMovieFragment())
         myAdapter.addFragmentToList(SessionsMovieFragment())
 
-        viewPager2.adapter = myAdapter
+        binding.viewPager.adapter = myAdapter
 
         TabLayoutMediator(
-            tabLayout,
-            viewPager2
+            binding.tablayout,
+            binding.viewPager
         ) { tab, position ->
             tab.text = tabsArray[position]
         }.attach()
     }
 
-    private fun argumentsPassed(){
-        val movieTitle = arguments?.getString("movieTitle")?: "Unknown Title"
-        binding.movieTitleTv.text = movieTitle
+
+    private fun setupView() {
+        binding.movieTitleTv.text = sharedViewModel.selectedMovieTitle.value
     }
+
+    private fun setupListeners() {
+        binding.backArrowTv.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
 
     // function used in about movie btn
     fun navigateToSeatSelection() {
