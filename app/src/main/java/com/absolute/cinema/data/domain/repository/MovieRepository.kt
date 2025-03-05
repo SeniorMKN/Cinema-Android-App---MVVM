56915c52e0961350468347585b2878e58f52a54f
jonathan.quaranta <jonathan.quaranta@synclab.it>
2025-03-05 18:40:27 +0100

    add auth token as constant

--- app/src/main/java/com/absolute/cinema/data/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/absolute/cinema/data/domain/repository/MovieRepository.kt
@@ -1,12 +1,11 @@
 package com.absolute.cinema.data.domain.repository
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.absolute.cinema.data.remote.MovieService
 import com.absolute.cinema.data.remote.Movies
 import com.absolute.cinema.data.remote.RetrofitInstance
 import com.absolute.cinema.data.remote.response.MovieDto
+import com.absolute.cinema.ui.utils.AUTH_TOKEN
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Response
@@ -18,7 +17,7 @@ class MovieRepository {
         return withContext(Dispatchers.IO) {
             try {
                 val response: Response<Movies> = movieService.getMovies(
-                    token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzJmY2UxYTBmN2U2M2U5ZGJmOTk1NWRjMTI3Zjg5OSIsIm5iZiI6MTc0MDc0MTkxNy4yMTYsInN1YiI6IjY3YzE5ZDFkYzY3YjM2OGJhNjM1OGJkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5LgeFIL123PAbki7cBEsybyGR55kkpzYJtqJxEbkYc8"
+                    token = AUTH_TOKEN
                 )
 
                 if (response.isSuccessful) {
--- app/src/main/java/com/absolute/cinema/ui/utils/Constants.kt
+++ app/src/main/java/com/absolute/cinema/ui/utils/Constants.kt
@@ -1,3 +1,4 @@
 package com.absolute.cinema.ui.utils
 
 const val BASE_BACKGROUND_IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
+const val AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzJmY2UxYTBmN2U2M2U5ZGJmOTk1NWRjMTI3Zjg5OSIsIm5iZiI6MTc0MDc0MTkxNy4yMTYsInN1YiI6IjY3YzE5ZDFkYzY3YjM2OGJhNjM1OGJkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5LgeFIL123PAbki7cBEsybyGR55kkpzYJtqJxEbkYc8"
