7f325da5e1f6c3d824db32ef19a88b8b4c61840d
Salvio96 <venerusofamily@hotmail.it>
2025-03-16 14:08:03 +0100

    adding selected movie title to the fragment

--- app/src/main/java/com/absolute/cinema/ui/pay/PayFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/pay/PayFragment.kt
@@ -3,7 +3,6 @@ package com.absolute.cinema.ui.pay
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -72,6 +71,8 @@ class PayFragment : Fragment() {
             sharedViewModel.getSelectedDate(),
             sharedViewModel.getSelectedTime()
         )
+
+        binding.movieTitleTv.text = sharedViewModel.getSelectedMovieTitle()
 
         binding.payContinueBtn.apply {
             isEnabled = false
