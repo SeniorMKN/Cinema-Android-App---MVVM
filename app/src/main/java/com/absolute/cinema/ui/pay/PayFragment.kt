8f96be6ba07f9b745a39dad4832fdc21d4d098d6
Salvio96 <venerusofamily@hotmail.it>
2025-03-18 17:52:47 +0100

    adding MovieTicket to db

--- app/src/main/java/com/absolute/cinema/ui/pay/PayFragment.kt
+++ app/src/main/java/com/absolute/cinema/ui/pay/PayFragment.kt
@@ -3,24 +3,32 @@ package com.absolute.cinema.ui.pay
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.absolute.cinema.R
+import com.absolute.cinema.data.local.movie.MovieDatabase
+import com.absolute.cinema.data.local.movie.MovieTicket
+import com.absolute.cinema.data.local.movie.MovieTicketDao
 import com.absolute.cinema.data.remote.MoviesSharedViewModel
 import com.absolute.cinema.databinding.FragmentPayBinding
 import com.absolute.cinema.ui.utils.UiUtils
 import com.absolute.cinema.ui.utils.UiUtils.initGridLayout
 import com.absolute.cinema.ui.utils.onBackPressed
+import kotlinx.coroutines.launch
 
 class PayFragment : Fragment() {
 
     private var _binding: FragmentPayBinding? = null
     private val binding get() = _binding!!
     private val sharedViewModel: MoviesSharedViewModel by activityViewModels()
+    private lateinit var db: MovieDatabase
+    private lateinit var ticketDao: MovieTicketDao
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,7 +50,21 @@ class PayFragment : Fragment() {
 
     private fun setupListeners() {
 
+        db = MovieDatabase.getDatabase(requireContext())
+        ticketDao = db.movieDao
+
         binding.payContinueBtn.setOnClickListener {
+            val ticket = MovieTicket(
+                movieId = sharedViewModel.getSelectedMovieId(),
+                ticketDate = sharedViewModel.getSelectedDate(),
+                movieTitle = sharedViewModel.getSelectedMovieTitle(),
+                cinemaName = sharedViewModel.getSelectedCinema()
+            )
+
+            lifecycleScope.launch {
+                ticketDao.insert(ticket)
+            }
+
             it.findNavController().navigate(R.id.action_payFragment_to_ticketFragment)
         }
 
