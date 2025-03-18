72faca654e68caea4b8e2120927bf4679653b188
Salvio96 <venerusofamily@hotmail.it>
2025-03-18 17:51:29 +0100

    adding MovieDatabase

--- app/src/main/java/com/absolute/cinema/data/local/movie/MovieDatabase.kt
+++ app/src/main/java/com/absolute/cinema/data/local/movie/MovieDatabase.kt
@@ -1,12 +1,29 @@
 package com.absolute.cinema.data.local.movie
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(
-    entities = [MovieEntity::class],
-    version = 1
-)
+@Database(entities = [MovieTicket::class],version = 1, exportSchema = false)
+
 abstract class MovieDatabase : RoomDatabase() {
-    abstract val movieDao: MovieDao
+    abstract val movieDao: MovieTicketDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MovieDatabase? = null
+
+        fun getDatabase(context: Context): MovieDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MovieDatabase::class.java,
+                    "movie_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
