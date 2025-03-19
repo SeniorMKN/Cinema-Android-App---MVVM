13c0cc77038015f8a6dd313d0c70fd83ce110555
Salvio96 <venerusofamily@hotmail.it>
2025-03-19 11:55:42 +0100

    small changes

--- app/src/main/java/com/absolute/cinema/ui/adapters/HistoryRecyclerViewAdapter.kt
+++ app/src/main/java/com/absolute/cinema/ui/adapters/HistoryRecyclerViewAdapter.kt
@@ -1,9 +1,9 @@
 package com.absolute.cinema.ui.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.absolute.cinema.R
 import com.absolute.cinema.data.model.HistoryItemModel
 import com.absolute.cinema.databinding.RecyclerHistoryLayoutBinding
 import com.absolute.cinema.ui.utils.BASE_BACKGROUND_IMAGE_PATH
@@ -17,9 +17,11 @@ class HistoryRecyclerViewAdapter(private val itemList: ArrayList<HistoryItemMode
 
         fun bind(item: HistoryItemModel) {
             binding.apply {
-                Glide.with(root.context)
+                Glide.with(movieImageIv.context)
                     .load(BASE_BACKGROUND_IMAGE_PATH + item.movieImage)
-                    .into(binding.movieImageIv)
+                    .placeholder(R.drawable.logo)
+                    .into(movieImageIv)
+
                 movieNameTv.text = item.movieName
                 movieDateTv.text = item.movieDate
                 cinemaNameTv.text = item.cinemaName
